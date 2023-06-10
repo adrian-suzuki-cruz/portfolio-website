50240c4551cf84165d71804d99dfe93b31da84bf
eichan06 <adrian.cruz06@gmail.com>
2023-06-10 10:01:33 -0700

    updated hamburger

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hello, welcome to my UX portfolio, A quick shoutout to my UX mentor, Ardie, who helped me learn how to build this website.");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
