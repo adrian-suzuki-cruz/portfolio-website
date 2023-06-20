f0946c7d918a66f1625fdd06af012070ab42303f
eichan06 <adrian.cruz06@gmail.com>
2023-06-19 20:03:12 -0700

    updated index and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,28 @@ function menuToggle() {
     x.className = "navtoggle";
   }
 }
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle scroll event
+function handleScroll() {
+  var workItems = document.querySelectorAll('.work-item');
+  
+  for (var i = 0; i < workItems.length; i++) {
+    if (isInViewport(workItems[i])) {
+      workItems[i].classList.add('show');
+    }
+  }
+}
+
+// Event listener for scroll event
+window.addEventListener('scroll', handleScroll);
